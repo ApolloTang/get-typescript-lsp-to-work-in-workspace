6f872144c7cb5c7a741af4ab58ed693a501247d1
Apollo Tang <tang.apollo@gmail.com>
2021-04-07 23:54:32 -0400

    initial commit

---
+++ need-transpile-before/packages/a/src/index.ts
@@ -0,0 +1,3 @@
+import {b} from 'b'
+              //^^^ [tsserver 2307] Cannot find module 'b' or its corresponding type declarations. [E]
+console.log(b('x'))
---
+++ need-transpile-before/packages/b/src/index.ts
@@ -0,0 +1,3 @@
+const b = (x:string):string => x
+export {b}
+
